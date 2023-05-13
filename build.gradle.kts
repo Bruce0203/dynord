4a3e411d9f4489d21ce7b583f0e734cb8b862e54
Bruce0203 <BruceFreedy@gmail.com>
2023-05-13 21:26:12 +0900

    style: clean up build script code

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,19 +11,14 @@ repositories {
     mavenCentral()
 }
 
-val koin_version: String by project
-
 dependencies {
     testImplementation(kotlin("test"))
     testImplementation(kotlin("reflect"))
-    compileOnly("io.insert-koin:koin-core:$koin_version")
-    testCompileOnly("io.insert-koin:koin-core:$koin_version")
 
     implementation("com.google.guava:guava:31.1-jre")
 
-    jmh("commons-io:commons-io:2.11.0")
-    jmh("org.openjdk.jmh:jmh-core:0.9")
-    jmh("org.openjdk.jmh:jmh-generator-annprocess:0.9")
+    jmh("org.openjdk.jmh:jmh-core:1.35")
+    jmh("org.openjdk.jmh:jmh-generator-annprocess:1.35")
 }
 
 tasks.test {
